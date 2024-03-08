a549c4c3db6945b0ea130ec7d1aeed351b9d3d40
Shoro749 <romanbro749@gmail.com>
2024-03-08 19:59:45 +0200

    Add project files.

---
+++ AutomicPartAdoNet/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using AutomicPartAdoNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutomicPartAdoNet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                //DataTable dataTable = new DataTable(nameof(BankAccount));
+
+                //DataColumn idColumn = new DataColumn("Id", typeof(int))
+                //{
+                //    AutoIncrement = true,
+                //    AutoIncrementSeed = 1,
+                //    AutoIncrementStep = 1,
+                //};
+                //DataColumn nameColumn = new DataColumn("Name", typeof(string));
+                //DataColumn cardColumn = new DataColumn("Card", typeof(string));
+                //DataColumn moneyColumn = new DataColumn("Balance", typeof(int));
+
+                //dataTable.Columns.Add(idColumn);
+                //dataTable.Constraints.Add(new UniqueConstraint(idColumn, true));
+                //dataTable.Columns.Add(nameColumn);
+                //dataTable.Columns.Add(cardColumn);
+                //dataTable.Columns.Add(moneyColumn);
+
+
+                //DataTable dataTable = TableGenerator.CreateTable(nameof(BankAccount), new BankAccount());
+
+                //DataRow row = dataTable.NewRow();
+                //row["Name"] = "John";
+                //row["Card"] = "1234-3423-6567-3434";
+                //row[3] = 1000;
+
+                //dataTable.Rows.Add(row);
+
+                //DataRow row1 = dataTable.NewRow();
+                //row1["Name"] = "Mark";
+                //row1["Card"] = "4354-3423-6567-3434";
+                //row1[3] = 2000;
+
+                //dataTable.Rows.Add(row1);
+
+                //dataGrid.ItemsSource = dataTable.DefaultView;
+
+                DataTable dataTable = TableGenerator.CreateTable(nameof(User), new User());
+
+                //DataRow row = dataTable.NewRow();
+                //row["Name"] = "John";
+                //row["Login"] = "john@gmail.com";
+                //row["Password"] = "qwerty";
+
+                //dataTable.Rows.Add(row);
+
+                //DataRow row1 = dataTable.NewRow();
+                //row1["Name"] = "Marc";
+                //row1["Login"] = "marc@gmail.com";
+                //row1["Password"] = "1234";
+
+                //dataTable.Rows.Add(row1);
+                TableGenerator.LoadToTable(dataTable, connectionString);
+
+                dataGrid.ItemsSource = dataTable.DefaultView;
+
+                //BankAccount bankAccount = new BankAccount()
+                //{
+                //    Id = 1,
+                //    Name = "Jhon",
+                //    Card = "2432-4324-3243-2434",
+                //    Balance = 5000,
+                //};
+                //string data = TableGenerator.ShowDataInfo(bankAccount);
+                //tbData.Text = data;
+            }
+        }
+    }
+}
---
+++ AutomicPartAdoNet/TableGenerator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AutomicPartAdoNet
+{
+    internal static class TableGenerator
+    {
+        public static DataTable CreateTable<T>(string tableName, T obj)
+        {
+            var columns = GetColumnsFromClass(obj);
+            DataTable table = new DataTable(tableName);
+            foreach(DataColumn column in columns)
+            {
+                table.Columns.Add(column);
+                if(column.ColumnName.ToLower() == "id")
+                {
+                    column.AutoIncrement = true;
+                    column.AutoIncrementSeed = 1;
+                    column.AutoIncrementStep = 1;
+                    table.Constraints.Add(new UniqueConstraint(column, true));
+                }
+            }
+
+            return table;
+        }
+
+        public static string ShowDataInfo<T>(T obj)
+        {
+            string data = "";
+
+            Type type = typeof(T);
+            data += $"Class name: {type.Name}\nParent class: {type.BaseType}\n";
+            foreach(var prop in type.GetProperties())
+            {
+                data += $"Prop Name: {prop.Name}, Type: {prop.PropertyType}, Value: {prop.GetValue(obj, null)}\n";
+            }
+            foreach(var prop in type.GetFields())
+            {
+                data += $"Field Name: {prop.Name}, Type: {prop.FieldType}, Value: {prop.GetValue(obj)}\n";
+            }
+            foreach(var method in type.GetMethods())
+            {
+                data += $"Method Name: {method.Name}, Return: {method.ReturnType}, IsPublic: {method.IsPublic}\n";
+            }
+
+            return data;
+        }
+
+        public static DataColumn[] GetColumnsFromClass<T>(T obj)
+        {
+            Type type = typeof(T);
+            List<DataColumn> list = new List<DataColumn>();
+
+            foreach(var prop in type.GetProperties())
+            {
+                DataColumn column = new DataColumn(prop.Name, prop.PropertyType);
+                list.Add(column);
+            }
+
+            return list.ToArray();
+        }
+
+        public static void AddRow<T>(DataTable table, T obj)
+        {
+            DataRow row = table.NewRow();
+            Type type = typeof(T);
+
+            foreach (var prop in type.GetProperties())
+            {
+                row[prop.Name] = prop.GetValue(obj, null);
+            }
+
+            table.Rows.Add(row);
+        }
+
+        public static void LoadToTable(DataTable table, string connectionString)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter($"select * from {table.TableName}", connection);
+                        sqlDataAdapter.Fill(table);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
